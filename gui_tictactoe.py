ce8969b42f8c277e131421c93dddb924ede6682e
notascii <kearzzcodeur@gmail.com>
2021-05-02 20:10:25 +0200

    save options + new buttons images

--- gui_tictactoe.py
+++ gui_tictactoe.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from game import Game
+import json
 
 class Application(object):
     def __init__(self, w, h):
@@ -9,7 +10,7 @@ class Application(object):
         self.pawns = {"white":[], "yellow":[], "black":[]}
         self.end_images = []
         self.buttons = []
-        self.pawns_theme = "black"
+        self.pawns_theme = "white" # A CHANGER ENTRE DANS LA LISTE DE COULEURS POSSIBLES
         self.themes = {"black":"white", "white":"black"}
 
     ####################
@@ -26,7 +27,7 @@ class Application(object):
         self.root.resizable(0, 0)
     
     def create_frame_and_canvas(self):
-        """ """
+        """utils"""
 
         self.topframe = tk.Frame(self.root)
         self.topframe.pack()
@@ -35,7 +36,7 @@ class Application(object):
         self.canvasGrid.pack()
 
     def create_pawns_images(self):
-        """ """
+        """utils"""
 
         for couleur in ["white", "yellow", "black"]:
             for symbol in ["croix", "rond"]:
@@ -43,28 +44,37 @@ class Application(object):
                     self.pawns[couleur].append(tk.PhotoImage(file=f"images/{symbol}_{taille}_{couleur}.png"))
     
     def create_end_images(self):
-        """ """
+        """utils"""
 
         self.end_images.append(tk.PhotoImage(file="images/end_croix_win.png"))
+        self.end_images.append(tk.PhotoImage(file="images/end_rond_win.png"))
 
 
     def display_menu_buttons(self):
         """Affiche les boutons du menu sur root."""
 
-        global buttonPlayImage, buttonPlay, buttonQuitImage, buttonQuit, buttonRules, buttonParameters
+        global buttonPlayImage, buttonPlay, buttonLoad, buttonLoadImage, buttonRules, buttonRulesImage, buttonQuitImage, buttonQuit
 
         buttonPlayImage = tk.PhotoImage(file="images/buttonPlay.png")
         buttonPlay = tk.Button(self.root, image=buttonPlayImage, relief="flat", command=self.play)
         pack_button(buttonPlay)
         self.buttons.append(buttonPlay)
 
-        buttonRules = tk.Button(self.root, relief="flat", text="Règles", command=self.rules)
+        buttonLoadImage = tk.PhotoImage(file="images/buttonLoad.png")
+        buttonLoad = tk.Button(self.root, image=buttonLoadImage, relief="flat", command=self.load_game)
+        pack_button(buttonLoad)
+        self.buttons.append(buttonLoad)
+
+        buttonRulesImage = tk.PhotoImage(file="images/buttonRules.png")
+        buttonRules = tk.Button(self.root, image=buttonRulesImage, relief="flat", command=self.rules)
         pack_button(buttonRules)
         self.buttons.append(buttonRules)
 
+        """ PAS LE TEMPS DE REMETTRE EN ETAT
         buttonParameters = tk.Button(self.root, relief="flat", text="Paramètres", command=self.parameters)
         pack_button(buttonParameters)
         self.buttons.append(buttonParameters)
+        """
 
         buttonQuitImage = tk.PhotoImage(file="images/buttonQuit.png")
         buttonQuit = tk.Button(self.root, image=buttonQuitImage, relief="flat", command=self.root.destroy)
@@ -80,7 +90,7 @@ class Application(object):
         Elle cache les boutons, créé le canvas, affiche la grille sur le canvas
         et associe le clic gauche de la souris à la fonction clic."""
 
-        global smallButtonQuitImage
+        global smallButtonSaveImage, smallButtonReplayImage, smallButtonQuitImage
 
         self.create_frame_and_canvas()
         self.create_pawns_images()
@@ -91,6 +101,7 @@ class Application(object):
         hide_menu_buttons()
 
         self.display_grid()
+        self.draw_all_pawns()
 
         # associe le bouton clic gauche à la fonction clic
         self.root.bind("<Button 1>", clic)
@@ -99,15 +110,33 @@ class Application(object):
         self.bottom_frame = tk.Frame()
         self.bottom_frame.pack(side=tk.BOTTOM)
 
-        small_button_save = tk.Button(self.bottom_frame, text="Sauvegarder", relief="flat")
+        smallButtonSaveImage = tk.PhotoImage(file="images/buttonSave_small.png")
+        small_button_save = tk.Button(self.bottom_frame, image=smallButtonSaveImage, relief="flat", command=self.save)
         small_button_save.pack(side=tk.LEFT)
 
-        small_button_replay = tk.Button(self.bottom_frame, text="Rejouer", relief="flat", command=self.replay)
+        smallButtonReplayImage = tk.PhotoImage(file="images/buttonReplay_small.png")
+        small_button_replay = tk.Button(self.bottom_frame, image=smallButtonReplayImage, relief="flat", command=self.replay)
         small_button_replay.pack(side=tk.LEFT)
         
         smallButtonQuitImage = tk.PhotoImage(file="images/buttonQuit_small.png")
         small_button_quit = tk.Button(self.bottom_frame, image=smallButtonQuitImage, relief="flat", command=self.root.destroy)
         small_button_quit.pack(side=tk.LEFT)
+    
+    def load_game(self):
+        """Charge la partie."""
+
+        savefile = open("save_tictactoe.json", "r")
+        data = json.load(savefile)
+        savefile.close()
+
+        game.grid = data["grid"]
+        game.subgrids = data["subgrids"]
+        game.n = int(data["n"])
+        game.pre_n = int(data["pre_n"])
+        game.pre_l = int(data["pre_l"])
+        game.pre_c = int(data["pre_c"])
+
+        self.play()
     
     def rules(self):
         """Cette fonction s'éxécute lorsque l'on clique sur le bouton Règles."""
@@ -131,8 +160,9 @@ class Application(object):
         returnButton = tk.Button(self.root, text="Retour", command=local_return_to_menu)
         returnButton.pack()
     
+    """ PAS LE TEMPS DE LA REMETTRE EN ETAT
     def parameters(self):
-        """Cette fonction s'éxécute lorsque l'on clique sur le bouton Paramètres."""
+        # Cette fonction s'éxécute lorsque l'on clique sur le bouton Paramètres.
 
         def local_return_to_menu():
             returnButton.pack_forget()
@@ -143,47 +173,71 @@ class Application(object):
 
         hide_menu_buttons()
 
-        """
-        radioButtonBlack = tk.Radiobutton(self.root, text="Noir", variable=self.pawns_theme, value="black")
-        radioButtonBlack.pack(expand=True)
-
-        radioButtonWhite = tk.Radiobutton(self.root, text="Blanc", variable=self.pawns_theme, value="white")
-        radioButtonWhite.pack(expand=True)
-        """
-
         black_pawns_button = tk.Button(self.root, text="Noir", command=lambda: self.change_color("black"))
         black_pawns_button.pack()
 
         white_pawns_button = tk.Button(self.root, text="Blanc", command=lambda: self.change_color("white"))
         white_pawns_button.pack()
 
-        b1 = tk.Button(self.root, text="click me", command=self.issou)
-        b1.pack()
-
         returnButton = tk.Button(self.root, text="Retour", command=local_return_to_menu)
         returnButton.pack(side=tk.BOTTOM)
-
-    def issou(self):
-        print(self.pawns_theme)
-    
+    """
+   
     def change_color(self, color):
+        """utils"""
         self.pawns_theme = color
 
     def replay(self):
-        """ """
+        """Reset la partie, nettoie le canvas, redisplay la grille de jeu et
+        rebind le bouton-1 de la souris à la fonction clic."""
 
         game.reset()
         self.canvasGrid.delete("all")
         self.display_grid()
         self.root.bind("<Button 1>", clic)
 
-    def end(self):
-        """ """
+        if game.is_subgrid_win_or_full(game.n):
+            # on colorie tous contours possible
+            for subgrid_nb in range(1,10):
+                if not game.is_subgrid_win_or_full(subgrid_nb):
+                    app.draw_outline(subgrid_nb, "red", 3)
+        else:
+            app.draw_outline(game.n, "red", 3)
 
-        self.canvasGrid.create_image(250, 250, image=app.end_images[0])
-        # arrêter le jeu
+
+    def end(self):
+        """Affiche un message lorsque la partie a été gagnée et unbind le bouton-1
+        de la souris."""
+
+        if game.is_grid_win("x"):
+            self.canvasGrid.create_image(250, 250, image=app.end_images[0])
+        if game.is_grid_win("o"):
+            self.canvasGrid.create_image(250, 250, image=app.end_images[1])
 
         self.root.unbind("<Button-1>")
+
+    def save(self):
+        """Sauvegarde la partie dans le fichier save_tictactoe.json"""
+
+        for symbol in ["x", "o"]:
+            if game.is_grid_win(symbol):
+                print("Vous ne pouvez pas sauvegarder une partie déjà gagnée.")
+                return -1
+
+        savefile = open("save_tictactoe.json", "r")
+        data = json.load(savefile)
+        savefile.close()
+
+        data["grid"] = game.grid
+        data["subgrids"] = game.subgrids
+        data["n"] = str(game.n)
+        data["pre_n"] = str(game.pre_n)
+        data["pre_l"] = str(game.pre_l)
+        data["pre_c"] = str(game.pre_c)
+
+        savefile = open("save_tictactoe.json", "w")
+        json.dump(data, savefile, indent=4)
+        savefile.close()
 
 
     ############################
@@ -220,9 +274,49 @@ class Application(object):
             # canvas.create_line(xc-60, yc-60, xc+60, yc+60, fill="white", width=10)
             # canvas.create_line(xc-60, yc+60, xc+60, yc-60, fill="white", width=10)
             self.canvasGrid.create_image(xc, yc, image=self.pawns[color][1])
+    
+    def draw_all_pawns(self):
+        """Dessine l'état de la partie actuelle."""
+
+        game.player = 0
+        for n in range(1, 10):
+            for l in range(0, 3):
+                for c in range(0, 3):
+                    i, j = convert_nlc_to_ij(n, l, c)
+                    x = 50*j + 25 + app.gapx
+                    y = 50*i + 25 + app.gapy
+
+                    if game.subgrids[str(n)][3*l + c] == "x":
+                        self.draw_pawn_on_canvas(x, y, 0, None)
+                        game.player = (game.player + 1)%2
+                    if game.subgrids[str(n)][3*l + c] == "o":
+                        self.draw_pawn_on_canvas(x, y, 1, None)
+                        game.player = (game.player + 1)%2
+
+        game.player = (game.player + 1)%2
+        i, j = convert_nlc_to_ij(game.pre_n, game.pre_l, game.pre_c)
+        x = 50*(j - 1) + 25 + app.gapx
+        y = 50*(i - 1) + 25 + app.gapy
+        self.draw_pawn_on_canvas(x, y, game.player, "yellow")
+        game.player = (game.player + 1)%2
+
+        for n in range(0, 9):
+            if game.grid[n] == "x":
+                self.draw_big_pawn_on_canvas(n + 1, 0, app.pawns_theme)
+            if game.grid[n] == "o":
+                self.draw_big_pawn_on_canvas(n + 1, 1, app.pawns_theme)
+        
+        if game.is_subgrid_win_or_full(game.n):
+            # on colorie tous contours possible
+            for subgrid_nb in range(1,10):
+                if not game.is_subgrid_win_or_full(subgrid_nb):
+                    app.draw_outline(subgrid_nb, "red", 3)
+        else:
+            app.draw_outline(game.n, "red", 3)
+
 
     def draw_outline(self, n, color, width):
-        """ """
+        """Repasse le contour d'une sous-grille."""
 
         if n == 1:
             x, y = 0, 0
@@ -233,7 +327,7 @@ class Application(object):
         self.canvasGrid.create_rectangle(x + self.gapx + 3, y + self.gapx + 3, x + 150 + self.gapy - 3, y + 150 + self.gapy - 3, outline=color, width=width)
     
     def clear_outline(self):
-        """ """
+        """Repasse le contour de toutes les sous-grilles."""
         
         for i in range(1,10):
             self.draw_outline(i, self.pawns_theme, 3)
@@ -253,6 +347,7 @@ class Application(object):
             self.canvasGrid.create_line(150*i+gapx, gapy, 150*i+gapx, 450+gapy, width=9, fill=self.pawns_theme) # lignes verticales
 
     def is_clic_in_canvas(self, x, y):
+        """utils"""
         if not ((self.gapx <= x <= self.gapx + 450) and (self.gapy <= y <= self.gapy + 450)):
             # le clic n'est pas dans le canvas
             return False
@@ -267,8 +362,9 @@ def hide_menu_buttons():
     """Cache les boutons Play, Rules, Parameters et Quit."""
 
     hide_button(buttonPlay)
+    hide_button(buttonLoad)
     hide_button(buttonRules)
-    hide_button(buttonParameters)
+    #hide_button(buttonParameters)
     hide_button(buttonQuit)
 
 def hide_button(button):
@@ -283,9 +379,13 @@ def pack_button(button):
 
 
 def get_grid_position(x, y):
+    """utils"""
+
     return ((x - 25) // 50, (y - 25) // 50)
 
 def convert_ij_to_nlc(i, j):
+    """utils"""
+
     case_number = 9*j + i + 1 # numéro de la case : de 1 à 81.
     n = 3*(j // 3) + i // 3 + 1
 
@@ -302,6 +402,8 @@ def convert_ij_to_nlc(i, j):
     return (n, l, c)
 
 def convert_nlc_to_ij(n, l, c):
+    """utils"""
+
     i = ((n - 1)//3)*3 + l
     j = ((n - 1)%3)*3 + c
     return (i, j)
@@ -321,6 +423,7 @@ def is_subgrid_or_grid_win(n, symbol, color):
             game.end = True
 
 def update_l_and_c(game, l, c):
+    """utils"""
     game.pre_l = l
     game.pre_c = c
 
@@ -342,8 +445,6 @@ def clic(event):
     n, l, c = convert_ij_to_nlc(i, j) # les convertir dans le format (numéro de sous-grille,ligne,colonne)
     key = str(n)
 
-    # print(f"A pre={game.pre_n} actuel={game.n} ({game.pre_l},{game.pre_c})")
-
     # détermine si le joueur peut poser un pion là où il a cliqué
     if game.is_possible(n, l, c):
         app.clear_outline()
@@ -354,11 +455,12 @@ def clic(event):
         # et s'il l'a remporté on vérifie s'il a remporté la grille
         is_subgrid_or_grid_win(n, symbol, app.pawns_theme)
 
+        # si un coup a déjà été jouée, on dessine par dessus le dernier coup
+        # en blanc pour effacer le pion jaune.
         if not (game.pre_l == 0) and not (game.pre_c == 0):
             pre_i, pre_j = convert_nlc_to_ij(game.pre_n, game.pre_l, game.pre_c)
             pre_x = 50*(pre_j - 1) + 25 + app.gapx
             pre_y = 50*(pre_i - 1) + 25 + app.gapy
-            # print(f"(i,j)=({pre_i},{pre_j}), (x,y)=({pre_x},{pre_y})\n")
             app.draw_pawn_on_canvas(pre_x, pre_y, (game.player+1)%2, app.pawns_theme)
 
         game.player = (game.player + 1)%2
@@ -368,7 +470,7 @@ def clic(event):
         update_l_and_c(game, l , c)
 
         if game.is_subgrid_win_or_full(game.n):
-            # on colorie tous contours possible en vert
+            # on colorie tous contours possible
             for subgrid_nb in range(1,10):
                 if not game.is_subgrid_win_or_full(subgrid_nb):
                     app.draw_outline(subgrid_nb, "red", 3)
@@ -378,70 +480,11 @@ def clic(event):
         if game.end:
             app.end()
 
-        # print(f"B pre={game.pre_n} actuel={game.n} ({game.pre_l},{game.pre_c})\n")
-
-
-
-        #####   #####
-          #     #   #
-          #     #####
-          #     #   #
-        #####   #   #
-
-        if game.player == 1 and is_ia == True: # joueur rond = IA
-            
-            if not (game.pre_l == 0) and not (game.pre_c == 0):
-                pre_i, pre_j = convert_nlc_to_ij(game.pre_n, game.pre_l, game.pre_c)
-                pre_x = 50*(pre_j - 1) + 25 + gapx
-                pre_y = 50*(pre_i - 1) + 25 + gapy
-                # print(f"(i,j)=({pre_i},{pre_j}), (x,y)=({pre_x},{pre_y})\n")
-                app.draw_pawn_on_canvas(pre_x, pre_y, (game.player+1)%2, app.pawns_theme)
-
-            game.n, l_ia, c_ia = ia.best_move(game, scores) # où joue l'ia ?
-            i_ia, j_ia = convert_nlc_to_ij(game.n, l_ia, c_ia)
-            x_ia = 50*(j_ia - 1) + 25 + gapx
-            y_ia = 50*(i_ia - 1) + 25 + gapy
-
-            """print(f"l_ia = {l_ia}, c_ia = {c_ia}")
-            print(f"game.n = {game.n} | n = {n}")
-            print("symbol =", symbol)
-            print(x_ia, y_ia)"""
-
-            app.clear_outline()
-            app.draw_pawn_on_canvas(x_ia, y_ia, game.player, "white")
-            print("symbol =", symbol)
-            game.subgrid_place(game.n, l_ia, c_ia, symbol)
-
-            is_subgrid_or_grid_win(n, "o", app.pawns_theme)
-            if game.is_subgrid_full(n):
-                print(f"La grille {n} est pleine.")
-            
-            # game.player vaut 1 mais je mets tjrs des croix
-            # dans game.subgrids
-            game.player = (game.player + 1)%2
-            game.pre_n = game.n
-            game.n = 3*(l_ia - 1) + c_ia
-
-            update_l_and_c(game, l_ia , c_ia)
-
-            if game.is_subgrid_win_or_full(game.n):
-                # on colorie tous contours possible en vert
-                for subgrid_nb in range(1,10):
-                    if not game.is_subgrid_win_or_full(subgrid_nb):
-                        app.draw_outline(subgrid_nb, "red", 3)
-            else:
-                app.draw_outline(game.n, "red", 3)
-            
-        # print(game.subgrids)
-
 
 if __name__ == "__main__":
 
     app = Application(500, 550)
     game = Game()
-
-    app.pawns_theme = "black"
-    scores = ia.Scores()
 
     app.create_root(app.geometry["width"], app.geometry["heigth"])
 
